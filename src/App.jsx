d2d1e3a102b67b3cfb791d27dffcfc26b6b03711
Afuh Christian <afuhchristian10@gmail.com>
2024-02-22 10:08:48 +0100

    fixed an app rendering issue .

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import BookRide from './Pages/BookRide';
 import { useSelector } from 'react-redux';
 import { LoadingIcon } from './Utils/Components/LoadingIcon';
 import { useDispatch } from 'react-redux';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import Cookies from 'js-cookie';
 
 import { RefreshUser, setEntry } from './ReduxStore/AuthSlice';
@@ -34,6 +34,20 @@ function App() {
  const user_ = useSelector(state => state.auth_reducer.user_)
  const network_status = useSelector(state => state.auth_reducer.network_status)
  const runOnce = useRef(false)
+
+ const LoadPage = useMemo(()=>{
+  if(network_status.refresh === "fulfilled"){
+    return RouterPage(entry)
+  }else {
+    return (
+      <div className="w-full h-screen grid place-content-center">
+<LoadingIcon/>
+</div>
+    )
+  }
+ } , [network_status.refresh , entry])
+
+
     useEffect(()=>{
       // console.log(Cookies.get("clientKlandoRefreshToken"))
       if(runOnce.current === false){
@@ -56,38 +70,52 @@ function App() {
     } , [network_status.refresh])
 
 // if(network_status.refresh === "pending") return <LoadingIcon/>
+// const network_status = useSelector(state => state.auth_reducer.network_status)
+// if(network_status.refresh === "pending") return (<div className="w-full h-screen grid place-content-center">
+// <LoadingIcon/>
+// </div>)
 
   return (
-    <> 
-     <BrowserRouter basename = '/'>
-      <Routes>
-        {
-          entry  ? 
-          <Route path='/' element={  <MainLayout />}>
-          <Route index element={ <HomePage /> } />
-          <Route path='clients' element={<ClientsPage />} />
-          <Route path='account' element={<AccountPage />} />
-          <Route path='availableRides' element={<AvailableRides />} />
-          <Route path='bookRide' element={<BookRide />} />
-          {/* <Route path='products' element={<Products />} /> */}
-          {/* <Route path='products/:productId' element={<SingleProduct />} /> */}
-          {/* <Route path='*' element={<Error />} /> */}
-        </Route>
-         : 
-        <>
-         <Route path='/' element = {<AppOpen/>}/>
-         <Route  element = {<BackLayout/>}>
-         <Route path='signUp' element = {<Auth_SignUp/>}/>
-         <Route path='login' element = {<Auth_Login/>}/>
-         </Route>
-         </>
-        } 
-         <Route path='*' element = {<Page404/>}/>
-      </Routes>
-    </BrowserRouter>
-    </>
+   <>
+   {LoadPage}
+   </>
   )
 }
+
+
+
+function RouterPage(entry){
+  return (  <> 
+<BrowserRouter basename = '/'>
+ <Routes>
+   {
+     entry ?
+     <Route path='/' element={  <MainLayout />}>
+     <Route index element={ <HomePage /> } />
+     <Route path='clients' element={<ClientsPage />} />
+     <Route path='account' element={<AccountPage />} />
+     <Route path='availableRides' element={<AvailableRides />} />
+     <Route path='bookRide' element={<BookRide />} />
+     {/* <Route path='products' element={<Products />} /> */}
+     {/* <Route path='products/:productId' element={<SingleProduct />} /> */}
+     {/* <Route path='*' element={<Error />} /> */}
+   </Route>
+    : 
+   <>
+    <Route path='/' element = {<AppOpen/>}/>
+    <Route  element = {<BackLayout/>}>
+    <Route path='signUp' element = {<Auth_SignUp/>}/>
+    <Route path='login' element = {<Auth_Login/>}/>
+    </Route>
+    </>
+   } 
+    <Route path='*' element = {<Page404/>}/>
+ </Routes>
+</BrowserRouter>
+</>
+  )
+}
+
 
 
 export default App
